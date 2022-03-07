cd2851de01fe1a5a3bbbabcad977e56fb3e65682
Tulcharamkethavath <101092982+Tulcharamkethavath@users.noreply.github.com>
2022-03-07 14:46:52 +0530

    Greater number

---
+++ sun.java
@@ -0,0 +1,14 @@
+class sun
+{
+	public static void main(String[] args)	
+	{
+		int a=10 , b=30 ;
+		if(a>b){		
+			System.out.println("a is greater than b");
+		}
+		else
+		{
+			System.out.println("b is greater than a");
+			}
+	}	
+}
